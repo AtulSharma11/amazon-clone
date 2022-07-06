59090a7def5c3a3cb04760b0d78101906b15e025
Atul <atulsharma.sa1@gmail.com>
2022-07-06 17:09:15 -0400

    Created navbar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import CheckoutPage from "./pages/CheckoutPage";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/login" element={<LoginPage />} />
-
         {/* This the default Route */}
         <Route path="/" element={<HomePage />} />
       </Routes>
---
+++ src/Header.js
@@ -0,0 +1,64 @@
+import "./Header.css";
+import { Link } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+
+function Header() {
+  return (
+    <nav className="header">
+      {/* Logo on the left */}
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        />
+      </Link>
+
+      {/* Search box */}
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      {/* 3 Links */}
+      <div className="header__nav">
+        {/* 1st Link */}
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello, User</span>
+            <span className="header__optionLineTwo">Sign In</span>
+          </div>
+        </Link>
+
+        {/* 2nd Link */}
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Retuens</span>
+            <span className="header__optionLineTwo">Orders</span>
+          </div>
+        </Link>
+
+        {/* 3rd Link */}
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
+        </Link>
+
+        {/* 4th Link */}
+        <Link to="/checkout" className="header__link">
+          <div className="header__optionBasket">
+            {/* Shopping basket icon */}
+            <ShoppingBasketIcon />
+            {/* Number of item in the basket */}
+            <span className="header__optionLineTwo header__basketCount">0</span>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
