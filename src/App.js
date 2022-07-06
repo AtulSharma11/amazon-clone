2fed593f4b5c5f42721c44cd3191b2ebba4c90fa
Atul <atulsharma.sa1@gmail.com>
2022-07-06 14:57:57 -0400

    Updating to (Router V6)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CheckoutPage from "./pages/CheckoutPage";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <div className="app">
-        <Switch>
-          <Route path="/checkout">
-            <h1>Checkout</h1>
-          </Route>
+      <Routes>
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/login" element={<LoginPage />} />
 
-          <Route path="/login">
-            <h1>Login Page</h1>
-          </Route>
-
-          {/* This the default Route */}
-          <Route path="/">
-            <h1>Home Page</h1>
-          </Route>
-        </Switch>
-      </div>
+        {/* This the default Route */}
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </Router>
   );
 }
---
+++ src/pages/CheckoutPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CheckoutPage() {
+  return (
+    <div>
+      <h1>This is the checkout page</h1>
+    </div>
+  );
+}
+
+export default CheckoutPage;
