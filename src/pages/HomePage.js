bbf22408e9a424960fe18cce6a0d7ba1d3d6a6a0
Atul <atulsharma.sa1@gmail.com>
2022-07-11 21:22:46 -0400

    Created Product component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,67 @@
 import React from "react";
 import "./HomePage.css";
+import Product from "./Product.js";
 
 function HomePage() {
   return (
     <div className="home">
       <img
         className="home__image"
-        src="https://assets.aboutamazon.com/dims4/default/282b15a/2147483647/strip/true/crop/1698x956+1+0/resize/1320x743!/format/webp/quality/90/?url=https%3A%2F%2Famazon-blogs-brightspot.s3.amazonaws.com%2F07%2Ff9%2F93aa1f3e4e259a046604ed3579d7%2Fbest-amazon-prime-day-deals-2022.jpg"
+        src="https://m.media-amazon.com/images/I/61RI8ywLDwL._SX3000_.jpg"
         alt=""
       />
+      {/* Product id ,title ,price , rating, image */}
+      <div className="home__row">
+        <Product
+          id="12422342"
+          title="2021 Apple MacBook Pro (14-inch, Apple M1 Pro chip with 8 core CPU and 14 core GPU, 16GB RAM, 512GB SSD) - Space Grey - English"
+          price={2499.99}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/61vFO3R5UNL._AC_SL1500_.jpg"
+        />
+        <Product
+          id="567576586789"
+          title="Xbox Series X - Xbox Series X Edition"
+          price={599.99}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/61vkgDi7wHL._AC_SX679_.jpg"
+        />
+      </div>
+
+      <div className="home__row">
+        <Product
+          id="6578567"
+          title="The Summer I Turned Pretty"
+          price={9.99}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/71j25Ib5FSL.jpg"
+        />
+        <Product
+          id="34577889"
+          title="Amazon Fire TV 55inch 4-Series 4K UHD smart TV"
+          price={439.99}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/61wCcgVn7-L._AC_SX679_.jpg"
+        />
+        <Product
+          id="0945567"
+          title="Harry'S House (Vinyl)"
+          price={38.98}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/71sZfX2A-lL._AC_SL1500_.jpg"
+        />
+        <div />
+      </div>
+      <div className="home__row">
+        <Product
+          id="5755567"
+          title="PlayStation 5 Console Disc Edition Horizon Forbidden West Bundle"
+          price={699.99}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/61NlIib88JL._AC_SL1500_.jpg"
+        />
+      </div>
+      {/* Product */}
     </div>
   );
 }
---
+++ src/pages/Product.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Product.css";
+
+function Product({ id, title, image, price, rating }) {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {/* Looping whatever number is passed inside the rating probs which is 5 and its gonna fill it with empty values and that its gonna map 5 times and gonna create 5 stars as a result */}
+          {Array(rating)
+            .fill()
+            .map((_) => (
+              <p>⭐</p>
+            ))}
+        </div>
+      </div>
+      <img src={image} alt="" />
+      <button>Add to basket</button>
+    </div>
+  );
+}
+
+export default Product;
