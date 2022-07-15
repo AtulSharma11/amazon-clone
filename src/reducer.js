36f1682e8e017d1258112ff7aca83f2078204ce4
Atul <atulsharma.sa1@gmail.com>
2022-07-14 22:15:42 -0400

    Set up the checkout page

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,14 @@
 export const initialState = {
-  basket: [],
+  basket: [
+    {
+      id: "12422342",
+      title:
+        "2021 Apple MacBook Pro (14-inch, Apple M1 Pro chip with 8 core CPU and 14 core GPU, 16GB RAM, 512GB SSD) - Space Grey - English",
+      price: 2499.99,
+      rating: 5,
+      image: "https://m.media-amazon.com/images/I/61vFO3R5UNL._AC_SL1500_.jpg",
+    },
+  ],
   user: null,
 };
 
